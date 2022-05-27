5a730052a824251403768dbd924377f5b355de69
stummk <stummk@users.noreply.github.com>
2022-05-27 18:56:52 +0200

    Update api.dart

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -109,28 +109,28 @@ class ApiService {
           if (statusCode == HttpStatus.unauthorized) {
             RequestOptions requestOptions = e.requestOptions;
 
-            try {
-              await _clientService.login();
-            } catch (e) {
-              // TODO check registration and authentication flow
-              // shopuld be implemented in issue #3 and #11
-              _store.delete(SecureStorageService.clientIdStorageKey);
-              _store.delete(SecureStorageService.clientSecretStorageKey);
-              _store.delete(SecureStorageService.rsaPrivateStorageKey);
-              _store.delete(SecureStorageService.appKeyStorageKey);
-              _store.delete(SecureStorageService.serverNameStorageKey);
-              RouterService.getInstance()
-                  .navigatorKey
-                  .currentState!
-                  .pushReplacementNamed('/register');
-            }
+            // TODO check registration and authentication flow
+            // shopuld be implemented in issue #3 and #11
+            // try {
+            //   await _clientService.login();
+            // } catch (e) {
+            //   _store.delete(SecureStorageService.clientIdStorageKey);
+            //   _store.delete(SecureStorageService.clientSecretStorageKey);
+            //   _store.delete(SecureStorageService.rsaPrivateStorageKey);
+            //   _store.delete(SecureStorageService.appKeyStorageKey);
+            //   _store.delete(SecureStorageService.serverNameStorageKey);
+            //   RouterService.getInstance()
+            //       .navigatorKey
+            //       .currentState!
+            //       .pushReplacementNamed('/register');
+            // }
 
-            if (!(await _store.has(
-              SecureStorageService.accessTokenStorageKey,
-            ))) {
-              // Logout was called, since token incorrect or expired!
-              return handler.reject(e);
-            }
+            // if (!(await _store.has(
+            //   SecureStorageService.accessTokenStorageKey,
+            // ))) {
+            //   // Logout was called, since token incorrect or expired!
+            //   return handler.reject(e);
+            // }
 
             // Retry request with the new token.
             var retryDio = Dio();
