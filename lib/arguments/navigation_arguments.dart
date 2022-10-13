6319d5f22007f6def155a4ed1bc431789c0bb239
stummk <stummk@users.noreply.github.com>
2022-10-13 20:15:42 +0200

    Update lib/arguments/navigation_arguments.dart
    Co-authored-by: Dennis Stumm <6422711+dennis95stumm@users.noreply.github.com>

--- lib/arguments/navigation_arguments.dart
+++ lib/arguments/navigation_arguments.dart
@@ -2,7 +2,7 @@ import 'package:mml_app/components/filter_app_bar.dart';
 
 /// Navigation arguments for the route navigation.
 class NavigationArguments {
-  /// The [FilterAppBar]]of the actual route.
+  /// The [FilterAppBar] of the actual route.
   final FilterAppBar appBar;
 
   /// Initializes the [NavigationArguments].
